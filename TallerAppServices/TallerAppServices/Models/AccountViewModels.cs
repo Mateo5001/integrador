fe9693dc98136c477a65100a3a113318712c8a70
Mateo5001 <danielomateo5001@hotmail.com>
2016-05-18 01:40:44 -0500

    cambion Interfaz login y validacion de usuario logueado

--- TallerAppServices/TallerAppServices/Models/AccountViewModels.cs
+++ TallerAppServices/TallerAppServices/Models/AccountViewModels.cs
@@ -10,10 +10,6 @@ namespace TallerAppServices.Models
         public string Email { get; set; }
     }
 
-    public class ExternalLoginListViewModel
-    {
-        public string ReturnUrl { get; set; }
-    }
 
     public class SendCodeViewModel
     {
@@ -49,35 +45,33 @@ namespace TallerAppServices.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
+        [Display(Name = "Usuario")]
         [EmailAddress]
-        public string Email { get; set; }
+        public string Usuario { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Contraseña")]
         public string Password { get; set; }
-
-        [Display(Name = "Remember me?")]
-        public bool RememberMe { get; set; }
+        
     }
 
     public class RegisterViewModel
     {
         [Required]
         [EmailAddress]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
+        [Display(Name = "Usuario")]
+        public string Usuario { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "La {0} debe contener {2} caracteres.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Contraseña")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "Confirme contraseña")]
+        [Compare("Password", ErrorMessage = "Las contraseñas ingresadas no corresponden")]
         public string ConfirmPassword { get; set; }
     }
 
