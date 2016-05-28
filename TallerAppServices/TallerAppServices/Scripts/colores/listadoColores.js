4c8fc11a41b8cffebbeaf39b8503cfbf3e9c1632
Mateo5001 <danielomateo5001@hotmail.com>
2016-05-27 19:05:41 -0500

    ultima

--- TallerAppServices/TallerAppServices/Scripts/colores/listadoColores.js
+++ TallerAppServices/TallerAppServices/Scripts/colores/listadoColores.js
@@ -1,7 +1,6 @@
 ﻿$(document).ready(function () {
     $("#idColor").change(function () {
         var data = $("#idColor").val();
-        alert(data);
         $.get('../colorDetalles/lista/' + data, function (data3) {
             $('#detalleColores').html(data3);
         })
